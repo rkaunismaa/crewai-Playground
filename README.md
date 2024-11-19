5c6a4c7ee77d5766097ec56571544b8529730741
rkaunismaa <rkaunis@gmail.com>
2024-11-19 18:57:39 -0500

    created demo_app and tested

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # crewai-Playground
+
 This will be my playground for all things to do with crewai.
+
+## Tuesday, November 19, 2024
+
+ 1) mamba create -n crewai python=3.11
+ 2) conda activate crewai
+ 3) mamba install conda-forge::jupyterlab
+ 4) mamba install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+ 5) pip install crewai  ... version 0.80.0 ... this installed a ton of stuff!! 
+ 6) pip install 'crewai[tools]' ... version 0.14.0
+
+We are going to step through the Matthew Berman video [Step-by-Step Langtrace + CrewAI Tutorial - Production Agent Stack](https://www.youtube.com/watch?v=dh9zv8EUwBA) as a start to this project.
+
+ Now we will generate a use crewai to create a demo app ... and just to be safe, I am going to run this command from the repository folder.
+
+ 7) crewai create crew demo-app
+
+ This asks me to select a provider (I selected ollama), and then a model (I selected ollama/llama3.1). This generates the 'demo_app' sub folder.
+
+ Next, we go into that 'demo_app' folder, and then run the command ....
+ 
+ 8) cd demo_app
+ 9) crewai install 
+
+ This again loads a ton of stuff, and once it has finished, you then run ...
+
+ 10) crewai run
+
+ Hmm ok something is not working, so how do we get this working with ollama??
+
+ OK Nice! I got the crew working with Ollama running locally! I am guessing getting it to work against KAUWITB should be as easy as setting the ip ...
+
+
+
+
+
+
+
---
+++ demo_app/src/demo_app/crew.py
@@ -0,0 +1,59 @@
+from crewai import Agent, Crew, Process, Task, LLM
+from crewai.project import CrewBase, agent, crew, task
+
+# Uncomment the following line to use an example of a custom tool
+# from demo_app.tools.custom_tool import MyCustomTool
+
+# Check our tools documentations for more information on how to use them
+# from crewai_tools import SerperDevTool
+
+llm=LLM(model="ollama/llama3.2", base_url="http://localhost:11434")
+# llm=LLM(model="ollama/llama3.2", base_url="http://192.168.2.16:11434")
+
+@CrewBase
+class DemoApp():
+	"""DemoApp crew"""
+
+	agents_config = 'config/agents.yaml'
+	tasks_config = 'config/tasks.yaml'
+
+	@agent
+	def researcher(self) -> Agent:
+		return Agent(
+			config=self.agents_config['researcher'],
+			# tools=[MyCustomTool()], # Example of custom tool, loaded on the beginning of file
+			verbose=True,
+			llm=llm
+		)
+
+	@agent
+	def reporting_analyst(self) -> Agent:
+		return Agent(
+			config=self.agents_config['reporting_analyst'],
+			verbose=True,
+			llm=llm
+		)
+
+	@task
+	def research_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['research_task'],
+		)
+
+	@task
+	def reporting_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['reporting_task'],
+			output_file='report.md'
+		)
+
+	@crew
+	def crew(self) -> Crew:
+		"""Creates the DemoApp crew"""
+		return Crew(
+			agents=self.agents, # Automatically created by the @agent decorator
+			tasks=self.tasks, # Automatically created by the @task decorator
+			process=Process.sequential,
+			verbose=True,
+			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+		)
